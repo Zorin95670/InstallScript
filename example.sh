d1829e6d1b772f3a624fd37f0ea67b191f50c536
Vincent <moittie.vincent@gmail.com>
2015-05-16 03:06:45 +0200

    Ajout du script de base

---
+++ example.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# ------------------------------------------------------------------------------------------------------------------------
+#                                               Script d'example d'installation de serveur
+#
+#	Auteur: 	Moittie Vincent
+#	Date:		2015
+#	OS :		Debian
+#	Version :	8
+#	Description:
+#			Description du script d'installation
+#			blabla
+#	Paramètre:	
+#			1° Test
+#			2° Test
+#			3° Test
+#-------------------------------------------------------------------------------------------------------------------------
+addressWeb=http://ADRESSE_DE_BASE/REPERTOIRE_PRINCIPALE
+
